b60db060d9f539ff18d94cec884926a4fbad9fdd
LeandroX01 <leandro.oliver_lima@hotmail.com>
2022-08-19 16:42:27 -0300

    Inclusão da framewoork de css CHACKRA UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from 'next'
+import { Button } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
    <>
+   <Button>
+     Clicou
+   </Button>
    </>
   )
 }
