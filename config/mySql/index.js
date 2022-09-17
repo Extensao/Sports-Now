57c0509c0960c2800d91ee7580f9a2e559cf9aed
LeandroX01 <leandro.oliver_lima@hotmail.com>
2022-09-16 23:27:27 -0300

    Envio da string de conexão com o banco como env

--- config/mySql/index.js
+++ config/mySql/index.js
@@ -1,15 +1,10 @@
 import mysql from 'mysql2';
 
 const connection =  mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "extensao_projeto",
-  password: "L1A2D3O20#"
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DB,
+  password: process.env.MYSQL_PWD
 })
-
-// MYSQL_HOST = "localhost"
-// MYSQL_USER = "root"
-// MYSQL_DB = "extensao_projeto"
-// MYSQL_PWD ="L1A2D3O20#"
 
 export {connection}
